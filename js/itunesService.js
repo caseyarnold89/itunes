275a3614178a265ff7578c3c0e2b90a932ecf094
Casey Arnold <caseyarnold89@gmail.com>
2016-02-19 22:49:23 -0700

    Completed all but last two of step 4. Need to look into filtering

--- js/itunesService.js
+++ js/itunesService.js
@@ -9,6 +9,33 @@ app.service('itunesService', function($http, $q){
   //Note that in the above line, artist is the parameter being passed in. 
   //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
 
-    //Code here
+  this.findArtist = function(artist) {
+      var deferred = $q.defer();
+      
+      $http({
+          method: 'JSONP',
+          url: 'https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
+      })
+      .then(function(response){
+          var firstResp = response.data.results; //an array of objects
+          console.log(firstResp);
+          var parseData = [];
+        //   var newObj = {};
+          for (var i = 0; i < firstResp.length; i++) {
+              var newObj = {};
+              newObj.Song = firstResp[i].trackName;
+              newObj.AlbumArt = firstResp[i].artworkUrl60;
+              newObj.Artist = firstResp[i].artistName;
+              newObj.Collection = firstResp[i].collectionName;
+              newObj.CollectionPrice = firstResp[i].collectionPrice;
+              newObj.Play = firstResp[i].previewUrl;
+              newObj.Type = firstResp[i].kind;
+              parseData.push(newObj);
+          }
+          deferred.resolve(parseData);
+      })
+      
+      return deferred.promise;
+  }
     
 });
